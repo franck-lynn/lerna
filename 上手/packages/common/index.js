68e12cbc2c12fdc82b59105e92acb3ae91d6ab0f
franck-lynn <lry_demry@163.com>
2021-11-21 23:07:07 +0800

    lerna

---
+++ 上手/packages/common/index.js
@@ -0,0 +1,6 @@
+const commonFn = () => {
+    console.log("hello from common, 修改一下")
+}
+export default commonFn
+
+// lerna add  @lynn/common --scope lerna-study
---
+++ 上手/packages/server/index.js
@@ -0,0 +1,13 @@
+// https://github.com/benawad/yarn-workspaces-example
+// https://www.bilibili.com/video/BV16a4y1h7R8?from=search&seid=16842717464687050876
+// 深入 lerna 发包机制 —— lerna version
+// https://zhuanlan.zhihu.com/p/354649322
+// lerna管理前端packages的最佳实践
+// http://www.sosout.com/2018/07/21/lerna-repo.html
+// https://www.youtube.com/watch?v=j0FiMekdeOs
+import  commonFn from '@lynn/common'
+commonFn()
+
+// git@github.com:franck-lynn/lerna-study.git
+// https://github.com/franck-lynn/lerna-study.git
+// git remote add origin  git@github.com:franck-lynn/lerna-study.git 
